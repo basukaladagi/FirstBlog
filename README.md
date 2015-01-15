e276696e4ca8af625c4d3a2fefd9a04eae041d19
Basavaraj K <basukaladagi@gmail.com>
2015-01-15 18:55:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple blogging engine..written for learning..
---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from blog.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['title', 'desc']
+	list_filter = ['published', 'created']
+	search_fields = ['title', 'desc','content']
+	data_hierarchy = 'created'
+	save_on_top = True
+	prepopulated_fields = {"slug" :("title",)}
+
+admin.site.register(Post)
